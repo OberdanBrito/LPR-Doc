1ab34cc9415563aae3b0936aaa25197e82b2a173
Oberdan Brito <oberdan@craos.net>
2019-08-01 16:05:02 -0300

    Inclusão do serviço websocket

--- server/index.js
+++ server/index.js
@@ -26,6 +26,20 @@ app.listen(port, () => {
     console.log(`Serviço de identificação LPR executando na porta ${port}.`)
 });
 
+const WebSocket = require('ws');
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(data);
+            }
+        });
+    });
+});
+
 app.post('/', (request, response) => {
 
     response.status(200).send();
@@ -44,6 +58,14 @@ app.post('/', (request, response) => {
         cor = vermelho;
     }
     console.log(cor, 'camera_id:' + request.body.camera_id + ' ' + results.plate + ' ' + results.confidence);
+    wss.clients.forEach(function (client) {
+       client.send(JSON.stringify({
+           filedate: new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"}),
+           camera_id: request.body.camera_id,
+           plate: results.plate,
+           confidence:results.confidence
+       }));
+    });
 
     let trace = request.body;
     delete trace.results;
@@ -60,3 +82,5 @@ app.post('/', (request, response) => {
 
 
 });
+
+let t = {"version":2,"data_type":"alpr_results","epoch_time":1564684472088,"img_width":1280,"img_height":720,"processing_time_ms":316.861176,"regions_of_interest":[],"results":[{"plate":"G8U8266","confidence":88.541321,"matches_template":0,"plate_index":0,"region":"br","region_confidence":0,"processing_time_ms":24.387512,"requested_topn":1,"coordinates":[{"x":563,"y":78},{"x":717,"y":74},{"x":719,"y":121},{"x":566,"y":125}],"candidates":[{"plate":"G8U8266","confidence":88.541321,"matches_template":0}]}],"uuid":"p1-cam1-1564684472408","camera_id":1,"site_id":"p1","company_id":"craos"}
