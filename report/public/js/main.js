926853e04b0047eed88878d62604e1898a767704
oberdan@craos.net <qe446pnh@>
2019-08-02 22:34:44 -0300

    Arquivo seletor de relatorios

--- report/public/js/main.js
+++ report/public/js/main.js
@@ -26,48 +26,4 @@ dhtmlxEvent(window, "load", function () {
         ]
     });
 
-    let list = layout.cells('a').attachList({
-        container: "data_container",
-        type: {
-            template: "<b>Data:</b>#filedate# <br/><b>Câmera:</b>#camera_id# <br/><b>Placa:</b> #plate#<br/><b>Nível de confiança:</b> #confidence#",
-            height: 'auto'
-        }
-    });
-
-    let wss = new WebSocket('ws://localhost:3000/');
-
-    wss.onopen = function (event) {
-        console.info('Websocket aberto com sucesso!', event);
-    };
-
-    wss.onclose = function (event) {
-        console.warn('Atenção o websocket está fechado', event);
-    };
-
-    wss.onerror = function (event) {
-        console.error('Erro websocket:', event);
-    };
-
-    wss.onmessage = function (event) {
-
-        if (event.type !== 'message')
-            return;
-
-        let data = JSON.parse(event.data);
-
-        hist.push(data);
-        list.add(data, 0);
-
-        layout.cells('b').attachURL('../storage/' + data.uuid, false, false);
-
-        list.attachEvent("onItemClick", function (id) {
-            hist.filter(function (item) {
-                if (item.id === id)
-                    layout.cells('b').attachURL(item.uuid, false, false);
-            });
-            return true;
-        });
-
-    };
-
 });
