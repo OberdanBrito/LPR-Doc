deb2334701875d8b769cf208fbbf5cd922853407
oberdan@craos.net <qe446pnh@>
2019-08-04 10:32:22 -0300

    Capacidade de armazenamento da estrutura JSON enviada pelo ALPR no banco de dados relacional

--- server/index.js
+++ server/index.js
@@ -43,24 +43,119 @@ wss.on('connection', function connection(ws) {
 
 app.post('/', (request, response) => {
 
-    console.debug(request.body);
     response.status(200).send();
+    insert_job(request);
+    
+});
 
-    let results = request.body.results[0];
+function insert_job(request) {
 
-    let verde = '\x1b[32m%s\x1b[0m';
-    let amarelo = '\x1b[33m%s\x1b[0m';
-    let vermelho = '\x1b[31m%s\x1b[0m';
-    let cor = null;
-    if (results.confidence > 88) {
-        cor = verde
-    } else if (results.confidence < 88) {
-        cor = amarelo;
-    } else if (results.confidence < 85) {
-        cor = vermelho;
-    }
-    console.log(cor, 'camera_id:' + request.body.camera_id + ' ' + results.plate + ' ' + results.confidence);
+    let
+        job = request.body,
+        regions_of_interest = request.body.regions_of_interest,
+        results = request.body.results;
+
+    show_plate(results);
+
+    delete job.regions_of_interest;
+    delete job.results;
+
+    db('job')
+        .insert(job)
+        .returning('id')
+        .then((result_job) => {
+
+            let job_id = result_job[0];
+
+            results.filter(function (item) {
+                item.job = job_id;
+                insert_results(item);
+            });
+
+            regions_of_interest.filter(function (item) {
+               item.job = job_id;
+                insert_regions(item);
+            });
+
+        });
+
+
+}
+
+function insert_results(request) {
+
+    let {
+        coordinates,
+        candidates
+    } = request;
+
+    delete request.coordinates;
+    delete request.candidates;
+
+    db('results')
+            .insert(request)
+            .returning('id')
+            .then((result) => {
+
+                let results_id = result[0];
+
+                coordinates.filter(function (item) {
+                    item.results = results_id;
+                    insert_coordinates(item);
+                });
+
+                candidates.filter(function (item) {
+                    item.results = results_id;
+                    insert_candidates(item);
+                });
+
+            });
+
+}
+
+function insert_coordinates(coordinate) {
+
+    db('coordinates')
+        .insert(coordinate)
+        .returning('id')
+        .then((result) => {
+            return result[0];
+        });
+
+}
+
+function insert_candidates(candidate) {
+
+    db('candidates')
+        .insert(candidate)
+        .returning('id')
+        .then((result) => {
+            return result[0];
+        });
+
+}
+
+
+function insert_regions(regions) {
+
+    if (!regions)
+        return;
+
+    regions.filter(function (item) {
+        db('results')
+            .insert(item)
+            .returning('id')
+            .then((result) => {
+                return result[0];
+            });
+    });
+
+}
+
+function send_ws_clients(request) {
+
     wss.clients.forEach(function (client) {
+
         client.send(JSON.stringify({
             id: request.body.epoch_time,
             filedate: new Date().toLocaleString("pt-BR", {timeZone: "America/Sao_Paulo"}),
@@ -70,19 +165,24 @@ app.post('/', (request, response) => {
             uuid:request.body.uuid
         }));
     });
+    
+}
 
-    let trace = request.body;
-    delete trace.results;
+function show_plate(result) {
 
-    trace.plate = results.plate;
-    trace.confidence = results.confidence;
+    let {
+        confidence,
+        plate
+    } = result[0];
 
-    db('trace')
-        .insert(trace)
-        .returning('id')
-        .then((result) => {
-            return result;
-        })
+    let cor = '\x1b[32m%s\x1b[0m', amarelo = '\x1b[33m%s\x1b[0m', vermelho = '\x1b[31m%s\x1b[0m';
+    
+    if (confidence < 88) {
+        cor = amarelo;
+    } else if (confidence < 85) {
+        cor = vermelho;
+    }
+    
+    console.log(cor, plate + ' ' + confidence);
 
-
-});
+}
