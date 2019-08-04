a28d4cbaaf588c915f9a5fd0a19ab6b96267973e
oberdan@craos.net <qe446pnh@>
2019-08-04 10:31:25 -0300

    Estrutura completa do banco de dados

--- install/slpr.sql
+++ install/slpr.sql
@@ -3,30 +3,9 @@
 --
 
 -- Dumped from database version 11.3
--- Dumped by pg_dump version 11.4 (Debian 11.4-1)
+-- Dumped by pg_dump version 11.3
 
-SET statement_timeout = 0;
-SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
-SET client_encoding = 'UTF8';
-SET standard_conforming_strings = on;
-SELECT pg_catalog.set_config('search_path', '', false);
-SET check_function_bodies = false;
-SET xmloption = content;
-SET client_min_messages = warning;
-SET row_security = off;
-
-DROP DATABASE IF EXISTS slpr;
---
--- Name: slpr; Type: DATABASE; Schema: -; Owner: postgres
---
-
-CREATE DATABASE slpr WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8';
-
-
-ALTER DATABASE slpr OWNER TO postgres;
-
-\connect slpr
+-- Started on 2019-08-04 10:28:38 -03
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -44,6 +23,7 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
+-- TOC entry 207 (class 1259 OID 33056)
 -- Name: candidates; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -59,6 +39,7 @@ CREATE TABLE public.candidates (
 ALTER TABLE public.candidates OWNER TO postgres;
 
 --
+-- TOC entry 206 (class 1259 OID 33054)
 -- Name: candidates_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -74,6 +55,8 @@ CREATE SEQUENCE public.candidates_id_seq
 ALTER TABLE public.candidates_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3182 (class 0 OID 0)
+-- Dependencies: 206
 -- Name: candidates_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -81,6 +64,7 @@ ALTER SEQUENCE public.candidates_id_seq OWNED BY public.candidates.id;
 
 
 --
+-- TOC entry 205 (class 1259 OID 33043)
 -- Name: coordinates; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -95,6 +79,7 @@ CREATE TABLE public.coordinates (
 ALTER TABLE public.coordinates OWNER TO postgres;
 
 --
+-- TOC entry 204 (class 1259 OID 33041)
 -- Name: coordinates_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -110,6 +95,8 @@ CREATE SEQUENCE public.coordinates_id_seq
 ALTER TABLE public.coordinates_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3183 (class 0 OID 0)
+-- Dependencies: 204
 -- Name: coordinates_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -117,10 +104,11 @@ ALTER SEQUENCE public.coordinates_id_seq OWNED BY public.coordinates.id;
 
 
 --
--- Name: jobs; Type: TABLE; Schema: public; Owner: postgres
+-- TOC entry 201 (class 1259 OID 32988)
+-- Name: job; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE public.jobs (
+CREATE TABLE public.job (
     filedate timestamp without time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     id bigint NOT NULL,
     version character varying(50),
@@ -139,6 +127,7 @@ CREATE TABLE public.jobs (
 ALTER TABLE public.job OWNER TO postgres;
 
 --
+-- TOC entry 200 (class 1259 OID 32986)
 -- Name: jobs_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -153,6 +142,8 @@ CREATE SEQUENCE public.jobs_id_seq
 ALTER TABLE public.jobs_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3184 (class 0 OID 0)
+-- Dependencies: 200
 -- Name: jobs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -160,18 +151,24 @@ ALTER SEQUENCE public.jobs_id_seq OWNED BY public.job.id;
 
 
 --
+-- TOC entry 203 (class 1259 OID 33021)
 -- Name: regions; Type: TABLE; Schema: public; Owner: postgres
 --
 
 CREATE TABLE public.regions (
     id integer NOT NULL,
-    job bigint
+    job bigint,
+    x integer,
+    y integer,
+    width integer,
+    weight integer
 );
 
 
 ALTER TABLE public.regions OWNER TO postgres;
 
 --
+-- TOC entry 202 (class 1259 OID 33019)
 -- Name: regions_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -187,6 +184,8 @@ CREATE SEQUENCE public.regions_id_seq
 ALTER TABLE public.regions_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3185 (class 0 OID 0)
+-- Dependencies: 202
 -- Name: regions_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -194,6 +193,7 @@ ALTER SEQUENCE public.regions_id_seq OWNED BY public.regions.id;
 
 
 --
+-- TOC entry 199 (class 1259 OID 32974)
 -- Name: results; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -215,6 +215,7 @@ CREATE TABLE public.results (
 ALTER TABLE public.results OWNER TO postgres;
 
 --
+-- TOC entry 198 (class 1259 OID 32972)
 -- Name: results_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -230,6 +231,8 @@ CREATE SEQUENCE public.results_id_seq
 ALTER TABLE public.results_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3186 (class 0 OID 0)
+-- Dependencies: 198
 -- Name: results_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -237,6 +240,7 @@ ALTER SEQUENCE public.results_id_seq OWNED BY public.results.id;
 
 
 --
+-- TOC entry 197 (class 1259 OID 32956)
 -- Name: trace; Type: TABLE; Schema: public; Owner: postgres
 --
 
@@ -262,6 +266,7 @@ CREATE TABLE public.trace (
 ALTER TABLE public.trace OWNER TO postgres;
 
 --
+-- TOC entry 196 (class 1259 OID 32954)
 -- Name: trace_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
@@ -277,6 +282,8 @@ CREATE SEQUENCE public.trace_id_seq
 ALTER TABLE public.trace_id_seq OWNER TO postgres;
 
 --
+-- TOC entry 3187 (class 0 OID 0)
+-- Dependencies: 196
 -- Name: trace_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
@@ -284,6 +291,7 @@ ALTER SEQUENCE public.trace_id_seq OWNED BY public.trace.id;
 
 
 --
+-- TOC entry 3041 (class 2604 OID 33059)
 -- Name: candidates id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -291,6 +299,7 @@ ALTER TABLE ONLY public.candidates ALTER COLUMN id SET DEFAULT nextval('public.c
 
 
 --
+-- TOC entry 3040 (class 2604 OID 33046)
 -- Name: coordinates id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -298,13 +307,15 @@ ALTER TABLE ONLY public.coordinates ALTER COLUMN id SET DEFAULT nextval('public.
 
 
 --
--- Name: jobs id; Type: DEFAULT; Schema: public; Owner: postgres
+-- TOC entry 3038 (class 2604 OID 32992)
+-- Name: job id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.job ALTER COLUMN id SET DEFAULT nextval('public.jobs_id_seq'::regclass);
 
 
 --
+-- TOC entry 3039 (class 2604 OID 33024)
 -- Name: regions id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -312,6 +323,7 @@ ALTER TABLE ONLY public.regions ALTER COLUMN id SET DEFAULT nextval('public.regi
 
 
 --
+-- TOC entry 3036 (class 2604 OID 32978)
 -- Name: results id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -319,6 +331,7 @@ ALTER TABLE ONLY public.results ALTER COLUMN id SET DEFAULT nextval('public.resu
 
 
 --
+-- TOC entry 3034 (class 2604 OID 32960)
 -- Name: trace id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
@@ -326,83 +339,7 @@ ALTER TABLE ONLY public.trace ALTER COLUMN id SET DEFAULT nextval('public.trace_
 
 
 --
--- Data for Name: candidates; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: coordinates; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: jobs; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: regions; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: results; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
-
---
--- Data for Name: trace; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-
---
--- Name: candidates_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.candidates_id_seq', 1, false);
-
-
---
--- Name: coordinates_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.coordinates_id_seq', 1, false);
-
-
---
--- Name: jobs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.jobs_id_seq', 1, false);
-
-
---
--- Name: regions_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.regions_id_seq', 1, false);
-
-
---
--- Name: results_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.results_id_seq', 1, false);
-
-
---
--- Name: trace_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('public.trace_id_seq', 4516, true);
-
-
---
+-- TOC entry 3051 (class 2606 OID 33061)
 -- Name: candidates candidates_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -411,6 +348,7 @@ ALTER TABLE ONLY public.candidates
 
 
 --
+-- TOC entry 3049 (class 2606 OID 33048)
 -- Name: coordinates coordinates_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -419,7 +357,8 @@ ALTER TABLE ONLY public.coordinates
 
 
 --
--- Name: jobs jobs_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
+-- TOC entry 3045 (class 2606 OID 33012)
+-- Name: job jobs_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.job
@@ -427,6 +366,7 @@ ALTER TABLE ONLY public.job
 
 
 --
+-- TOC entry 3047 (class 2606 OID 33026)
 -- Name: regions regions_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -435,6 +375,7 @@ ALTER TABLE ONLY public.regions
 
 
 --
+-- TOC entry 3043 (class 2606 OID 33016)
 -- Name: results results_pk; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -443,6 +384,7 @@ ALTER TABLE ONLY public.results
 
 
 --
+-- TOC entry 3055 (class 2606 OID 33062)
 -- Name: candidates foreignkey_candidates_results; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -451,6 +393,7 @@ ALTER TABLE ONLY public.candidates
 
 
 --
+-- TOC entry 3054 (class 2606 OID 33049)
 -- Name: coordinates foreignkey_coordinates_results; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
@@ -459,20 +402,24 @@ ALTER TABLE ONLY public.coordinates
 
 
 --
+-- TOC entry 3053 (class 2606 OID 33027)
 -- Name: regions foreignkey_regions_jobs; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.regions
-    ADD CONSTRAINT foreignkey_regions_jobs FOREIGN KEY (job) REFERENCES public.job (id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT foreignkey_regions_jobs FOREIGN KEY (job) REFERENCES public.job(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
 
 --
+-- TOC entry 3052 (class 2606 OID 33036)
 -- Name: results foreignkey_results_jobs; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
 ALTER TABLE ONLY public.results
-    ADD CONSTRAINT foreignkey_results_jobs FOREIGN KEY (job) REFERENCES public.job (id) ON UPDATE CASCADE ON DELETE CASCADE;
+    ADD CONSTRAINT foreignkey_results_jobs FOREIGN KEY (job) REFERENCES public.job(id) ON UPDATE CASCADE ON DELETE CASCADE;
 
+
+-- Completed on 2019-08-04 10:28:39 -03
 
 --
 -- PostgreSQL database dump complete
